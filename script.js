2c389069786cb9c9308f5d9be3baefe5132238cf
rafaelly-3a <160231003+rafaelly-3a@users.noreply.github.com>
2025-05-28 11:34:43 +0000

    finalizamos o site

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
     const alternaContraste = document.getElementById('alterna-contraste')
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+    botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -26,5 +28,10 @@ document.addEventListener('DOMContentLoaded', function(){
        alternaContraste.addEventListener('click', function(){
          document.body.classList.toggle('alto-contraste')
      })
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
 
 });
